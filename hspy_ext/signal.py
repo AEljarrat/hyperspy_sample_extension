6a753faf5e37de8905deb007406bef86ad23cf5d
Alberto Eljarrat <alberto.eljarrat@gmail.com>
2020-06-23 17:47:19 +0200

    New signal `MyEELSSpectrum`

--- hspy_ext/signal.py
+++ hspy_ext/signal.py
@@ -1,7 +1,16 @@
 from hyperspy._signals.signal1d import Signal1D
+from hyperspy._signals.eels import EELSSpectrum
 
 class MySignal(Signal1D):
     _signal_type = "MySignal"
+    _signal_dimension = 1
+
+    def create_model(self, dictionary=None):
+        from hspy_ext.model import MyModel
+        return MyModel(self, dictionary=dictionary)
+ 
+class MyEELSSpectrum(EELSSpectrum):
+    _signal_type = "MyEELSSignal"
     _signal_dimension = 1
 
     def create_model(self, dictionary=None):
